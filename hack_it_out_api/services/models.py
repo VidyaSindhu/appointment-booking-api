698aca3a6790af7add62cd9ee81a58a1b686ef27
VidyaSindhu <vidyasd08@gmail.com>
2021-10-16 21:47:44 +0530

    added Appointment ORM

--- hack_it_out_api/services/models.py
+++ hack_it_out_api/services/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from django.db.models.deletion import CASCADE
+from django.db.models.fields import DurationField
+from users.models import User
 # Create your models here.
+
+class Service(models.Model):
+    name = models.CharField(default="",max_length=255)
+    price = models.PositiveIntegerField()
+    duration = models.PositiveIntegerField()
+
+class Appointment(models.Model):
+    user = models.ForeignKey(User, on_delete=CASCADE)
+    doctor = models.ForeignKey(User, on_delete=CASCADE, related_name="doctor")
+    appointment_at = models.DateTimeField()
+    status = models.CharField(default="pending", max_length=255)
+    service = models.ForeignKey(Service, on_delete=CASCADE)
+    description = models.CharField(default=None, max_length=255)
